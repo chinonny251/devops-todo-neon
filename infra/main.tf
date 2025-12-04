95d82e88641ea91f50b83e6a00cb3c6f0bd7d020
Chinonso <chinonny.osigwe@gmail.com>
2025-12-04 10:35:02 -0800

    Update main.tf with Key Vault policy

--- infra/main.tf
+++ infra/main.tf
@@ -1,98 +1,106 @@
-<xaiArtifact identifier="complete-main-tf" type="text/plain" title="COMPLETE infra/main.tf - Copy & Replace Entire File"> ```hcl terraform { required_providers { azurerm = { source = "hashicorp/azurerm" version = "~>3.0" } } required_version = ">= 0.14.9" }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
 provider "azurerm" {
-features {}
+  features {}
 }
 
 resource "random_integer" "ri" {
-min = 10000
-max = 99999
+  min = 10000
+  max = 99999
 }
 
 resource "azurerm_resource_group" "rg" {
-name = "rg-devops-todo-${random_integer.ri.result}"
-location = "Canada Central"
+  name     = "rg-devops-todo-${random_integer.ri.result}"
+  location = "Canada Central"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-name = "vnet-devops-todo-${random_integer.ri.result}"
-address_space = ["10.0.0.0/16"]
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
+  name                = "vnet-devops-todo-${random_integer.ri.result}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-name = "subnet-devops-todo-${random_integer.ri.result}"
-resource_group_name = azurerm_resource_group.rg.name
-virtual_network_name = azurerm_virtual_network.vnet.name
-address_prefixes = ["10.0.1.0/24"]
+  name                 = "subnet-devops-todo-${random_integer.ri.result}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_key_vault" "kv" {
-name = "kv-devops-todo-${random_integer.ri.result}"
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
-tenant_id = data.azurerm_client_config.current.tenant_id
-sku_name = "standard"
+  name                = "kv-devops-todo-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
 }
 
-← NEW: Key Vault Access Policy (fixes 403 error)
 resource "azurerm_key_vault_access_policy" "devops_policy" {
-key_vault_id = azurerm_key_vault.kv.id
-tenant_id = data.azurerm_client_config.current.tenant_id
-object_id = data.azurerm_client_config.current.object_id
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
 
-secret_permissions = [
-"Get",
-"List",
-"Set",
-"Delete"
-]
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete"
+  ]
 }
 
 resource "azurerm_key_vault_secret" "database_url" {
-name = "database-url"
-value = var.database_url
-key_vault_id = azurerm_key_vault.kv.id
+  name         = "database-url"
+  value        = var.database_url
+  key_vault_id = azurerm_key_vault.kv.id
 }
 
 resource "azurerm_service_plan" "plan" {
-name = "asp-devops-todo-${random_integer.ri.result}"
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
+  name                = "asp-devops-todo-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-sku_name = "B1"
-os_type = "Linux"
+  sku_name = "B1"
+  os_type  = "Linux"
 }
 
 resource "azurerm_linux_web_app" "app" {
-name = "webapp-devops-todo-${random_integer.ri.result}"
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
-service_plan_id = azurerm_service_plan.plan.id
+  name                = "webapp-devops-todo-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.plan.id
 
-site_config {
-application_stack {
-node_version = "18-lts"
-}
-}
+  site_config {
+    application_stack {
+      node_version = "18-lts"
+    }
+  }
 
-app_settings = {
-"DATABASE_URL" = var.database_url
-"WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
-}
+  app_settings = {
+    "DATABASE_URL"                      = var.database_url
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
 
-connection_string {
-name = "DefaultConnection"
-type = "PostgreSQL"
-value = var.database_url
-}
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "PostgreSQL"
+    value = var.database_url
+  }
 }
 
 resource "azurerm_application_insights" "insights" {
-name = "appinsights-devops-todo-${random_integer.ri.result}"
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
-application_type = "web"
+  name                = "appinsights-devops-todo-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
 }
 
 data "azurerm_client_config" "current" {}
