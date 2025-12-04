5a4dbe4ba966aace92fe44f4a3ea7a39e2c564ce
Chinonso <chinonny.osigwe@gmail.com>
2025-12-03 20:56:18 -0800

    Fix main.tf syntax errors

--- infra/main.tf
+++ infra/main.tf
@@ -19,7 +19,7 @@ resource "random_integer" "ri" {
 
 resource "azurerm_resource_group" "rg" {
   name     = "rg-devops-todo-${random_integer.ri.result}"
-  location = "eastus"
+  location = "Canada Central"
 }
 
 resource "azurerm_virtual_network" "vnet" {
@@ -37,13 +37,11 @@ resource "azurerm_subnet" "subnet" {
 }
 
 resource "azurerm_key_vault" "kv" {
-  name                        = "kv-devops-todo-${random_integer.ri.result}"
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                   = "standard"
-  soft_delete_enabled         = true
-  purge_protection_enabled    = false
+  name                = "kv-devops-todo-${random_integer.ri.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
 }
 
 resource "azurerm_key_vault_secret" "database_url" {
@@ -52,44 +50,37 @@ resource "azurerm_key_vault_secret" "database_url" {
   key_vault_id = azurerm_key_vault.kv.id
 }
 
-resource "azurerm_app_service_plan" "plan" {
+resource "azurerm_service_plan" "plan" {
   name                = "asp-devops-todo-${random_integer.ri.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
-
-  kind = "Linux"
-  reserved = true
+  sku_name = "B1"
+  os_type  = "Linux"
 }
 
 resource "azurerm_linux_web_app" "app" {
   name                = "webapp-devops-todo-${random_integer.ri.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_app_service_plan.plan.id
+  service_plan_id     = azurerm_service_plan.plan.id
 
   site_config {
-    linux_fx_version = "NODE|18-lts" # Node 18 for Next.js
+    application_stack {
+      node_version = "~18"
+    }
   }
 
   app_settings = {
-    "DATABASE_URL" = var.database_url
+    "DATABASE_URL"                      = var.database_url
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
   }
-
-  https_only = true
 
   connection_string {
     name  = "DefaultConnection"
     type  = "PostgreSQL"
     value = var.database_url
   }
-
-  virtual_network_subnet_id = azurerm_subnet.subnet.id
 }
 
 resource "azurerm_application_insights" "insights" {
@@ -100,4 +91,3 @@ resource "azurerm_application_insights" "insights" {
 }
 
 data "azurerm_client_config" "current" {}
-
